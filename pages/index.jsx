5780894af5c8ba5d872c0a2b9f0180bea45e4905
BayuDC <dev.bayudc@gmail.com>
2022-11-28 08:32:27 +0700

    chore: style it with daisyui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
 function HomePage() {
-    return <h1 className="text-3xl font-bold underline">Welcome to Next.js!</h1>;
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Welcome to Next.js!</h1>
+            <button className="btn btn-primary">Welcome daisyui</button>
+        </>
+    );
 }
 
 export default HomePage;
