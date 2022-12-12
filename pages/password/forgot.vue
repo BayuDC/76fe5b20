5e0e8b8b634f95d4ea614888459fe42690fa4185
BayuDC <dev.bayudc@gmail.com>
2022-12-12 16:15:09 +0700

    feat: add forgot password page

---
+++ pages/password/forgot.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+definePageMeta({
+    layout: 'clear',
+});
+</script>
+
+<template>
+    <div class="hero min-h-screen">
+        <div class="hero-content text-center">
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Forgot Password?</h1>
+                <p class="py-6">
+                    Please contact the site administrator manually to reset your password. The developer of this website
+                    was too lazy to build this feature
+                </p>
+                <NuxtLink to="/login" class="btn btn-primary">Back to Login</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
