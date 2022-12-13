4f1f110652003fe440469586ce96ad51674e375b
BayuDC <dev.bayudc@gmail.com>
2022-12-13 17:10:49 +0700

    feat: display logged in indicator

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,12 +1,18 @@
+<script lang="ts" setup>
+const user = useAuthUser();
+</script>
+
 <template>
     <div class="navbar rounded-box p-0">
         <div class="flex-1">
             <NuxtLink to="/" class="link no-underline text-white text-4xl font-bold p-0">Tink!</NuxtLink>
         </div>
         <div class="flex-none">
-            <div class="dropdown dropdown-end">
+            <div v-if="user" class="dropdown dropdown-end">
                 <label tabindex="0" class="btn btn-ghost btn-circle avatar flex">
-                    <div class="w-10 rounded-full bg-primary text-white text-2xl font-bold leading-10">B</div>
+                    <div class="w-10 rounded-full bg-primary text-white text-2xl font-bold leading-10">
+                        {{ user.fullname[0] || user.username[0] }}
+                    </div>
                 </label>
                 <ul
                     tabindex="0"
@@ -20,6 +26,7 @@
                     </li>
                 </ul>
             </div>
+            <NuxtLink v-else to="/login" class="btn btn-primary">Login</NuxtLink>
         </div>
     </div>
 </template>
