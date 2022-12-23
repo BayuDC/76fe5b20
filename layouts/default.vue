97a8e96d3fd7113fd31a921086e8163db8521e83
BayuDC <dev.bayudc@gmail.com>
2022-12-23 20:56:13 +0700

    refactor: rename navbar to header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container max-w-6xl p-5 m-auto">
-        <Navbar />
+        <Header />
         <div class="divider mt-1 mb-10"></div>
         <main>
             <slot />
