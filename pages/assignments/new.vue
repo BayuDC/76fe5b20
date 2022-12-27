c5304db3ce1daab8fcbd647fad0d841fda0ff051
BayuDC <dev.bayudc@gmail.com>
2022-12-27 09:26:27 +0700

    chore: control the assignment form step

--- pages/assignments/new.vue
+++ pages/assignments/new.vue
@@ -10,16 +10,20 @@ const check = await useApi(`/courses/${courseId}/check`, { method: 'HEAD', toast
 setMenuItem([]);
 setMenuNav([{ label: data.value.course.name, href: `/courses/${courseId}` }, { label: 'New Assigment' }]);
 
-const FormGeneral = resolveComponent('AssignmentFormGeneral');
-const FormSchedule = resolveComponent('AssignmentFormSchedule');
+const position = ref(0);
+const Forms = [
+    resolveComponent('AssignmentFormGeneral'),
+    resolveComponent('AssignmentFormSchedule'),
+    resolveComponent('AssignmentFormAttachment'),
+];
 </script>
 
 <template>
     <div>
         <Course403 v-if="check.error.value" />
         <div v-else class="lg:flex lg:gap-8 lg:items-start">
-            <AssignmentSteps :position="1" />
-            <component :is="FormSchedule" :courseId="courseId" />
+            <AssignmentSteps :position="position" />
+            <component :is="Forms[position]" :courseId="courseId" @done="position++" />
         </div>
     </div>
 </template>
